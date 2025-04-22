3bbeb619ed7cf88718c4f6448e1167fdf9990d53
sebastien c. <schatel.pro@pm.me>
2025-04-22 09:05:35 +0200

    (feat) add function delete notes

--- model/functions.php
+++ model/functions.php
@@ -42,49 +42,68 @@
     //-- FUNCTION AFFICHER NOTES
     function AffichageNotes(){
 
-        // 1. Creation d'une variable globale qui pourra être accessible
+        // 1. Création d'une variable globale qui pourra être accessible
         global $bdd;
         
-        // 2. REQUETE SQL 
-        //SELECT `id`, `titre`, `texte` FROM `bloc-notes`
-        $sqlRequete = "SELECT `titre`, `texte` FROM `bloc-notes`";
+        // 2. Requête SQL pour récupérer les données
+        $sqlRequete = "SELECT `id`, `titre`, `texte` FROM `bloc-notes`";
         
-        // 3. Preparation de la requête
+        // 3. Préparation de la requête
         $PrepareRequete = $bdd->prepare($sqlRequete);
-
-        // 4. ENVOI REQUETE + VERIFICATION SI CELA FONCTIONNE CORRECTEMENT
-        if (!$PrepareRequete -> execute()){
-            echo "Requete SQL HS !";
+    
+        // 4. Envoi de la requête et vérification si ça fonctionne
+        if (!$PrepareRequete->execute()){
+            echo "Requête SQL HS !";
             exit;
         };
-
-        //RECUPERATION DU RESULTAT VIA FETCH ALL
-        //FETCH_ASSOC = Récupère une ligne de resultat sous forme d'un tableau associatif
-        $resultatsView = $PrepareRequete ->fetchAll(PDO::FETCH_ASSOC);
-
-        //BOUCLE DES DONNEES RECUPEREES VIA LA REQUETE SQL FETCH ALL
+    
+        // Récupération des résultats via FETCH ALL
+        $resultatsView = $PrepareRequete->fetchAll(PDO::FETCH_ASSOC);
+    
+        // BOUCLE DES DONNEES RECUPEREES VIA LA REQUETE SQL FETCH ALL
         foreach ($resultatsView as $datas){
             echo "<tr>";
             echo "<td>" . $datas['titre'] . "</td>";
             echo "<td>" . $datas['texte'] . "</td>";
+    
+            // 5. Ajout des liens de modification et de suppression
             echo "
             <td>
-            <a href='#' title='Cliquer pour modifier' class='cta-actions'>" . "Modifier" ."</a>
-            <a href='#' title='Cliquer pour supprimer' class='cta-actions'>" . "Supprimer" ."</a>
-            </td> ";
+                <a href='#' title='Cliquer pour modifier' class='cta-actions'>Modifier</a>
+                <a href='?action=supprimer&id=" . $datas['id'] . "' title='Cliquer pour supprimer' class='cta-actions'>Supprimer</a>
+            </td>";
             echo "</tr>";
-        };
+        }
     };
-
+    
     //-- FUNCTION SUPPRIMER NOTES
-
     function SupprimerNotes(){
 
-        echo "";
+        // 1. Creation d'une variable globale qui pourra être accessible
+        global $bdd;
 
+        // 2. Verification ID
+        // Si cette condition n'est pas présente alors message d'erreur que l'ID n'est pas défini.
+        //
+        if(isset($_GET['id'])) {
 
+            // 3. Securise la requête et récupère ID de l'Input
+            $id = $_GET['id'];
 
+            // 4. Requete SQL
+            $sqlRequete = "DELETE FROM `bloc-notes` WHERE `id` = :id";
+
+            // 5. Preparation de la requête
+            $PrepareRequete = $bdd->prepare($sqlRequete);
+
+            // 6. Lancement de la requête
+            $PrepareRequete ->execute([':id' => $id]);
+
+            // 7. Exit permet de clôturer la requête
+            exit;
+        }
     };
+
 
     //-- FUNCTION MODIFIER NOTES
 
